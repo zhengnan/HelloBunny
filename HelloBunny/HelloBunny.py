6d40d50bfc2e92df6312a831e14add0b70418c23
Focus <focus987519@gmail.com>
2013-11-03 23:02:29 +0800

    游戏基本完成
    加入声音、胜负判定，游戏可以运行！

--- HelloBunny/HelloBunny.py
+++ HelloBunny/HelloBunny.py
@@ -6,6 +6,7 @@ from pygame.locals import *
 
 #2 - Initialize the game
 pygame.init()
+pygame.mixer.init()
 width, height = 640, 480
 screen    = pygame.display.set_mode((width, height))
 keys      = [False, False, False, False]
@@ -24,9 +25,24 @@ castle = pygame.image.load("resources/images/castle.png")
 arrow  = pygame.image.load("resources/images/bullet.png")
 badguyimg1 = pygame.image.load("resources/images/badguy.png")
 badguyimg  = badguyimg1
-
+healthbar = pygame.image.load("resources/images/healthbar.png")
+health    = pygame.image.load("resources/images/health.png")
+gameover = pygame.image.load("resources/images/gameover.png")
+youwin   = pygame.image.load("resources/images/youwin.png") 
+#3.1 - Load audio
+hit = pygame.mixer.Sound("resources/audio/explode.wav")
+enemy = pygame.mixer.Sound("resources/audio/enemy.wav")
+shoot = pygame.mixer.Sound("resources/audio/shoot.wav")
+hit.set_volume(0.05)
+enemy.set_volume(0.05)
+shoot.set_volume(0.05)
+pygame.mixer.music.load('resources/audio/moonlight.wav')
+pygame.mixer.music.play(-1, 0.0)
+pygame.mixer.music.set_volume(0.25)
 #4 - keep looping through
-while 1:
+running = 1
+exit = 0
+while running:
 	#5 - clear the screen before drawing it again
 	screen.fill(0)
 	#6 - draw the screen elements
@@ -76,6 +92,7 @@ while 1:
 		badrect.top = badguy[1]
 		badrect.left = badguy[0]
 		if badrect.left < 64:
+			hit.play()
 			healthvalue -= random.randint(5, 20)
 			badguys.pop(index)
 		#index += 1
@@ -86,12 +103,24 @@ while 1:
 			bullrect.left = bullet[1]
 			bullrect.top = bullet[2]
 			if badrect.colliderect(bullrect):
+				enemy.play()
 				acc[0] += 1
 				badguys.pop(index)
 				arrows.pop(index1)
 			index1 += 1
 	for badguy in badguys:
 		screen.blit(badguyimg, badguy)
+	#6.4 - Draw clock
+	font = pygame.font.Font(None, 24)
+	survivedtext = font.render(str((90000 - pygame.time.get_ticks())/60000) + ":" 
+		+ str((90000 - pygame.time.get_ticks())/1000%60).zfill(2), True, (0, 0, 0))
+	textRect = survivedtext.get_rect()
+	textRect.topright = [635, 5]
+	screen.blit(survivedtext, textRect)
+	#6.5 - Draw health bar
+	screen.blit(healthbar, (5, 5))
+	for health1 in range(healthvalue):
+		screen.blit(health, (health1 + 8, 8))
 	#7 - update the screen
 	pygame.display.flip()
 	#8 - loop through the events
@@ -123,6 +152,7 @@ while 1:
 				keys[3] = False
 		
 		if event.type == pygame.MOUSEBUTTONDOWN:
+			shoot.play()
 			position = pygame.mouse.get_pos()
 			acc[1] += 1
 			arrows.append([math.atan2(position[1] - (playerpos1[1] + 32), 
@@ -139,3 +169,42 @@ while 1:
 		playerpos[0] += 5
 
 	badtimer -= 1
+	#10 - Win/Lose check
+	if pygame.time.get_ticks() >= 90000:
+		running = 0
+		exitcode = 1
+	if healthvalue <= 0:
+		running = 0
+		exitcode = 0
+	if acc[1] != 0:
+		accuracy = acc[0]*1.0/acc[1]*100
+	else:
+		accuracy = 0
+#11 - Win/Lose display
+	
+if exitcode == 0:
+	pygame.font.init()
+	font = pygame.font.Font(None, 24)
+	text = font.render("Accuracy:" + str(accuracy) + "%", 
+		True, (255, 0, 0))
+	textRect = text.get_rect()
+	textRect.centerx = screen.get_rect().centerx
+	textRect.centery = screen.get_rect().centery + 24
+	screen.blit(gameover, (0, 0))
+	screen.blit(text, textRect)
+else:
+	pygame.font.init()
+	font = pygame.font.Font(None, 24)
+	text = font.render("Accuracy:" + str(accuracy) + "%", 
+		True, (0, 255, 0))
+	textRect = text.get_rect()
+	textRect.centerx = screen.get_rect().centerx
+	textRect.centery = screen.get_rect().centery + 24
+	screen.blit(youwin, (0, 0))
+	screen.blit(text, textRect)
+while 1:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			exit(0)
+	pygame.display.flip()
